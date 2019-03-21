2f2766fd9a2abae3ab6e6769bb3fe55036d36004
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-03-21 00:50:33 +0000

    connect to mongdb

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 console.log('Server code running');
 
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 // serve files from the public directory
 app.use(express.static('public'));
+
+mongoose.connect('mongodb://localhost/clicks');
+mongoose.Promise = global.Promise;
 
 // start the express web server listening on 8080
 app.listen(8080, () => {
