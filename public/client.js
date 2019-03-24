5c1fb36a11d78722e6c95c1ad067718566ba36dc
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-03-24 21:09:50 +0000

    added set interval function that counts clicks after 1 second

--- public/client.js
+++ public/client.js
@@ -16,3 +16,17 @@ button.addEventListener('click', function(e) {
       console.log(error);
     });
 });
+
+setInterval(function() {
+  fetch('/clicks', {method: 'GET'})
+    .then(function(response) {
+      if(response.ok) return response.json();
+      throw new Error('Request failed.');
+    })
+    .then(function(data) {
+      document.getElementById('counter').innerHTML = `Button was clicked ${data.length} times`;
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}, 1000);
