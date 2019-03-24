c950150d11d39ae5cf1e7e1ef790b6f2ab0a9774
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-03-24 20:59:28 +0000

    modified client.js page

--- public/client.js
+++ public/client.js
@@ -4,7 +4,7 @@ const button = document.getElementById('myButton');
 button.addEventListener('click', function(e) {
   console.log('button was clicked');
 
-	  fetch('/clicked', {method: 'POST'})
+  fetch('/clicked', {method: 'POST'})
     .then(function(response) {
       if(response.ok) {
         console.log('Click was recorded');
