27a66ceee61d7113367b6ff93c474f1fd9eb38ff
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-03-24 20:55:17 +0000

    added mongoDB client to server.js

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
-console.log('Server code running');
+console.log('Server-side code running');
 
 const express = require('express');
-const mongoose = require('mongoose');
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
 
 // serve files from the public directory
 app.use(express.static('public'));
 
-mongoose.connect('mongodb://localhost/clicks');
-mongoose.Promise = global.Promise;
+// connect to the db and start the express server
+let db;
 
-// start the express web server listening on 8080
-app.listen(8080, () => {
-  console.log('listening on 8080');
+// ***Replace the URL below with the URL for your database***
+const url =  'mongodb://user:password@mongo_address:mongo_port/databaseName';
+// E.g. for option 2) above this will be:
+// const url =  'mongodb://localhost:21017/databaseName';
+
+MongoClient.connect(url, (err, database) => {
+  if(err) {
+    return console.log(err);
+  }
+  db = database;
+  // start the express web server listening on 8080
+  app.listen(8080, () => {
+    console.log('listening on 8080');
+  });
 });
 
 // serve the homepage
